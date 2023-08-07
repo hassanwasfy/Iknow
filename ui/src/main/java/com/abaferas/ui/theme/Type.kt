d6520c8725d2abca582081ddfd593d7c1e9bfd36
abaferas <wasfyelgamed@gmail.com>
2023-08-07 06:05:50 +0300

    implement typography design system

--- ui/src/main/java/com/abaferas/ui/theme/Type.kt
+++ ui/src/main/java/com/abaferas/ui/theme/Type.kt
@@ -2,33 +2,94 @@ package com.abaferas.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.abaferas.ui.R
 
-// Set of Material typography styles to start with
+val Poppins = FontFamily(
+    Font(R.font.poppins_light, weight = FontWeight.Light, style = FontStyle.Normal),
+    Font(R.font.poppins_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+    Font(R.font.poppins_medium, weight = FontWeight.Medium, style = FontStyle.Normal),
+    Font(R.font.poppins_semibold, weight = FontWeight.SemiBold, style = FontStyle.Normal),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold, style = FontStyle.Normal),
+    Font(R.font.poppins_extrabold, weight = FontWeight.ExtraBold, style = FontStyle.Normal),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
+        lineHeight = 72.sp,
+        letterSpacing = 0.5.sp
+    ), displayMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 48.sp,
+        letterSpacing = 0.5.sp
+    ), displaySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.5.sp
+    ), headlineLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+        lineHeight = 72.sp,
+        letterSpacing = 0.5.sp
+    ), headlineMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 48.sp,
+        letterSpacing = 0.5.sp
+    ), headlineSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.5.sp
+    ), bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        letterSpacing = 0.5.sp
+    ), bodyMedium = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ), bodySmall = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.5.sp
+    ), labelLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        letterSpacing = 0.5.sp
+    ), labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ), labelSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
