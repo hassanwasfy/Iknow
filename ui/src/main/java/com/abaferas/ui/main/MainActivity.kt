ba7d9be61a18d9fcacf89d01629569dee2c3219f
abaferas <wasfyelgamed@gmail.com>
2023-09-01 17:37:35 +0300

    implement design system base

--- ui/src/main/java/com/abaferas/ui/main/MainActivity.kt
+++ ui/src/main/java/com/abaferas/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.abaferas.ui
+package com.abaferas.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ ui/src/main/java/com/abaferas/ui/theme/Dimens.kt
@@ -0,0 +1,46 @@
+package com.abaferas.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimens(
+    val dimens1: Dp = 1.dp,
+    val dimens2: Dp = 2.dp,
+    val dimens8: Dp = 8.dp,
+    val dimens4: Dp = 4.dp,
+    val dimens12: Dp = 12.dp,
+    val dimens16: Dp = 16.dp,
+    val dimens24: Dp = 24.dp,
+    val dimens32: Dp = 32.dp,
+    val dimens36: Dp = 36.dp,
+    val dimens48: Dp = 48.dp,
+    val dimens49: Dp = 49.dp,
+    val dimens58: Dp = 58.dp,
+    val dimens78: Dp = 78.dp,
+    val dimens100: Dp = 100.dp,
+    val dimens56: Dp = 56.dp,
+    val dimens70: Dp = 70.dp,
+    val dimens80: Dp = 80.dp,
+    val dimens88: Dp = 88.dp,
+    val dimens104: Dp = 104.dp,
+    val dimens118: Dp = 118.dp,
+    val dimens112: Dp = 112.dp,
+    val dimens130: Dp = 130.dp,
+    val dimens140: Dp = 140.dp,
+    val dimens143: Dp = 143.dp,
+    val dimens176: Dp = 176.dp,
+    val dimens193: Dp = 193.dp,
+    val dimens270: Dp = 270.dp,
+    val dimens328: Dp = 328.dp,
+    val dimens365: Dp = 365.dp,
+)
+
+val LocalDimens = compositionLocalOf { Dimens() }
+val MaterialTheme.dimens: Dimens
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalDimens.current
