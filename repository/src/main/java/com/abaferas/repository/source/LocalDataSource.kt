dca46f57332acd4da220d7c58dbaa711b9a79f31
abaferas <wasfyelgamed@gmail.com>
2023-09-05 21:26:22 +0300

    remove room database

--- repository/src/main/java/com/abaferas/repository/source/LocalDataSource.kt
+++ repository/src/main/java/com/abaferas/repository/source/LocalDataSource.kt
@@ -1,29 +1,5 @@
 package com.abaferas.repository.source
 
-import com.abaferas.repository.models.entity.ArticleListEntity
-import com.abaferas.repository.models.entity.BooksEntity
-import com.abaferas.repository.models.entity.EntityArticle
-import com.abaferas.repository.models.entity.MostPopularArticleEntity
-import com.abaferas.repository.models.entity.TopStoriesEntity
-
 interface LocalDataSource {
-    suspend fun saveArticle(article: EntityArticle)
-    suspend fun getArticle(article: EntityArticle): List<EntityArticle>
-    suspend fun unSaveArticle(article: EntityArticle)
 
-    suspend fun saveBook(book: BooksEntity)
-    suspend fun getBook(book: BooksEntity): List<BooksEntity>
-    suspend fun unSaveBook(book: BooksEntity)
-
-    suspend fun saveList(list: ArticleListEntity)
-    suspend fun getList(list: ArticleListEntity): List<ArticleListEntity>
-    suspend fun unSaveList(list: ArticleListEntity)
-
-    suspend fun saveMostPopular(mostPopular: MostPopularArticleEntity)
-    suspend fun getMostPopular(mostPopular: MostPopularArticleEntity): List<MostPopularArticleEntity>
-    suspend fun unSaveMostPopular(mostPopular: MostPopularArticleEntity)
-
-    suspend fun saveTopStory(topStory: TopStoriesEntity)
-    suspend fun getTopStory(topStory: TopStoriesEntity): List<TopStoriesEntity>
-    suspend fun unSaveTopStory(topStory: TopStoriesEntity)
 }
