537653fae580965efa9464aed2bf87385a459bc2
abaferas <wasfyelgamed@gmail.com>
2023-09-16 14:20:11 +0300

    room dependencies inversion

---
+++ repository/src/main/java/com/abaferas/repository/repositories/ILocalRepositoryImpl.kt
@@ -0,0 +1,122 @@
+package com.abaferas.repository.repositories
+
+import com.abaferas.entities.Article
+import com.abaferas.entities.ArticleList
+import com.abaferas.entities.Books
+import com.abaferas.entities.MostPopularArticle
+import com.abaferas.entities.TopStories
+import com.abaferas.repository.mapper.toArticle
+import com.abaferas.repository.mapper.toArticleEntity
+import com.abaferas.repository.mapper.toArticleList
+import com.abaferas.repository.mapper.toArticleListEntity
+import com.abaferas.repository.mapper.toBooks
+import com.abaferas.repository.mapper.toBooksEntity
+import com.abaferas.repository.mapper.toMostPopularArticle
+import com.abaferas.repository.mapper.toMostPopularArticleEntity
+import com.abaferas.repository.mapper.toTopStories
+import com.abaferas.repository.mapper.toTopStoriesEntity
+import com.abaferas.repository.source.LocalDataSource
+import com.abaferas.usecase.repositories.ILocalRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class ILocalRepositoryImpl @Inject constructor(
+    private val localDataSource: LocalDataSource
+) : ILocalRepository {
+
+    override suspend fun insertArticle(article: Article) {
+        val articleEntity = article.docs[0].toArticleEntity()
+        localDataSource.insertArticle(articleEntity)
+    }
+
+    override suspend fun deleteArticle(article: Article) {
+        localDataSource.deleteArticle(article.docs[0].toArticleEntity())
+    }
+
+    override fun getAllArticles(): Flow<List<Article>> {
+        return localDataSource.getAllArticles().map { articleEntities ->
+            articleEntities.map { it.toArticle() }
+        }
+    }
+
+    override suspend fun getArticleById(id: Long): Flow<Article> {
+        return localDataSource.getArticleById(id.toString()).map { it.toArticle() }
+    }
+
+    override suspend fun insertBook(books: Books) {
+        val booksEntity = books.toBooksEntity()
+        localDataSource.insertBook(booksEntity)
+    }
+
+    override suspend fun deleteBook(books: Books) {
+        localDataSource.deleteBook(books.toBooksEntity())
+    }
+
+    override fun getAllBooks(): Flow<List<Books>> {
+        return localDataSource.getAllBooks().map { booksEntities ->
+            booksEntities.map { it.toBooks() }
+        }
+    }
+
+    override suspend fun getBookById(id: Long): Flow<Books> {
+        return localDataSource.getBookById(id).map { it.toBooks() }
+    }
+
+    override suspend fun insertArticleList(articleList: ArticleList) {
+        val articleListEntity = articleList.toArticleListEntity()
+        localDataSource.insertArticleList(articleListEntity)
+    }
+
+    override suspend fun deleteArticleList(articleList: ArticleList) {
+        localDataSource.deleteArticleList(articleList.toArticleListEntity())
+    }
+
+    override fun getAllArticleLists(): Flow<List<ArticleList>> {
+        return localDataSource.getAllArticleLists().map { articleListEntities ->
+            articleListEntities.map { it.toArticleList() }
+        }
+    }
+
+    override suspend fun getArticleListById(id: Long): Flow<ArticleList> {
+        return localDataSource.getArticleListById(id).map { it.toArticleList() }
+    }
+
+    override suspend fun insertMostPopularArticle(mostPopularArticle: MostPopularArticle) {
+        val mostPopularArticleEntity = mostPopularArticle.toMostPopularArticleEntity()
+        localDataSource.insertMostPopularArticle(mostPopularArticleEntity)
+    }
+
+    override suspend fun deleteMostPopularArticle(mostPopularArticle: MostPopularArticle) {
+        localDataSource.deleteMostPopularArticle(mostPopularArticle.toMostPopularArticleEntity())
+    }
+
+    override fun getAllMostPopularArticles(): Flow<List<MostPopularArticle>> {
+        return localDataSource.getAllMostPopularArticles().map { mostPopularArticleEntities ->
+            mostPopularArticleEntities.map { it.toMostPopularArticle() }
+        }
+    }
+
+    override suspend fun getMostPopularArticleById(id: Long): Flow<MostPopularArticle> {
+        return localDataSource.getMostPopularArticleById(id).map { it.toMostPopularArticle() }
+    }
+
+    override suspend fun insertTopStory(topStories: TopStories) {
+        val topStoriesEntity = topStories.toTopStoriesEntity()
+        localDataSource.insertTopStory(topStoriesEntity)
+    }
+
+    override suspend fun deleteTopStory(topStories: TopStories) {
+        localDataSource.deleteTopStory(topStories.toTopStoriesEntity())
+    }
+
+    override fun getAllTopStories(): Flow<List<TopStories>> {
+        return localDataSource.getAllTopStories().map { topStoriesEntities ->
+            topStoriesEntities.map { it.toTopStories() }
+        }
+    }
+
+    override suspend fun getTopStoryById(id: Long): Flow<TopStories> {
+        return localDataSource.getTopStoryById(id).map { it.toTopStories() }
+    }
+}
---
+++ usecase/src/main/java/com/abaferas/usecase/repositories/ILocalRepository.kt
@@ -0,0 +1,55 @@
+package com.abaferas.usecase.repositories
+
+import com.abaferas.entities.Article
+import com.abaferas.entities.ArticleList
+import com.abaferas.entities.Books
+import com.abaferas.entities.MostPopularArticle
+import com.abaferas.entities.TopStories
+import kotlinx.coroutines.flow.Flow
+
+interface ILocalRepository {
+    //region ArticleDao
+    suspend fun insertArticle(article: Article)
+
+    suspend fun deleteArticle(article: Article)
+
+    fun getAllArticles(): Flow<List<Article>>
+    suspend fun getArticleById(id: Long): Flow<Article>
+    //endregion
+
+    //region BooksDao
+    suspend fun insertBook(books: Books)
+
+    suspend fun deleteBook(books: Books)
+
+    fun getAllBooks(): Flow<List<Books>>
+    suspend fun getBookById(id: Long): Flow<Books>
+    //endregion
+
+    //region ArticleListDao
+    suspend fun insertArticleList(articleList: ArticleList)
+
+    suspend fun deleteArticleList(articleList: ArticleList)
+
+    fun getAllArticleLists(): Flow<List<ArticleList>>
+    suspend fun getArticleListById(id: Long): Flow<ArticleList>
+    //endregion
+
+    //region MostPopularDao
+    suspend fun insertMostPopularArticle(mostPopularArticle: MostPopularArticle)
+
+    suspend fun deleteMostPopularArticle(mostPopularArticle: MostPopularArticle)
+
+    fun getAllMostPopularArticles(): Flow<List<MostPopularArticle>>
+    suspend fun getMostPopularArticleById(id: Long): Flow<MostPopularArticle>
+    //endregion
+
+    //region TopStoryDao
+    suspend fun insertTopStory(topStories: TopStories)
+
+    suspend fun deleteTopStory(topStories: TopStories)
+
+    fun getAllTopStories(): Flow<List<TopStories>>
+    suspend fun getTopStoryById(id: Long): Flow<TopStories>
+    //endregion
+}
